56f8460839121114c26b36e8dcbd9e64654168a1
25noland <55511086+25noland@users.noreply.github.com>
2022-09-05 23:18:35 -0700

    Add files via upload

---
+++ ImpulseForce.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicsEngine
+{
+    public class ImpulseForce : Force
+    {
+        private Vector2 velocity;
+        private int impulseTime = 0;
+
+        public ImpulseForce(Vector2 velocity) : base(velocity)
+        {
+            this.velocity = -velocity * 7.5f; // Why does this constant behave so weirdly?
+        }
+
+        public override Vector2 getForce()
+        {
+            impulseTime++;
+            if (impulseTime <= 2)
+            {
+                return velocity;
+            }
+
+            finish();
+            return new Vector2(0, 0);
+        }
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PhysicsEngine
+{
+    public class Node
+    {
+        private Vector2 position;
+        private Vector2 velocity;
+
+        private Vector2 offset;
+        private float rotatedDegrees;
+
+        private SquareMesh parent;
+
+        public Node(SquareMesh parent, Vector2 position, Vector2 velocity, Vector2 offset)
+        {
+            this.position = position - offset;
+            this.offset = offset;
+            this.parent = parent;
+        }
+
+        public void Update(Vector2 position, Vector2 velocity, float rotation)
+        {
+            this.velocity = velocity;
+
+            Vector2 rotated = new Vector2((float) (offset.X * Math.Cos(rotation) - offset.Y * Math.Sin(rotation)), (float) (offset.X * Math.Sin(rotation) + offset.Y * Math.Cos(rotation)));
+
+            this.position =  position + rotated;
+        }
+        
+        public bool Collision(SquareMesh mesh)
+        {
+            if (position.Y > mesh.getNode(0).getPosition().Y && position.Y < mesh.getNode(2).getPosition().Y && position.X > mesh.getNode(0).getPosition().X && position.X < mesh.getNode(1).getPosition().X)
+            {
+                return true; // Calculate normal for better prediction
+            }
+            return false;
+        }
+
+        public void CollisionSolver(SquareMesh mesh)
+        {
+            if (Collision(mesh))
+            {
+                ImpulseForce force = new ImpulseForce(velocity);
+                parent.AddForce(force);
+            }
+        }
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+    }
+}
